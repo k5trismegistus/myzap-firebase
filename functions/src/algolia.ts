406ad8a2a883bb6ffce80dec5ad2df860b3e4276
keigo <k5.trismegistus@gmail.com>
2020-07-08 23:26:23 +0900

    :sparkles: Update algolia

--- functions/src/algolia.ts
+++ functions/src/algolia.ts
@@ -5,24 +5,26 @@ const ALGOLIA_ID = functions.config().algolia.app_id
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 
-const onSituationCreated = functions.firestore.document('situations/{situationId}').onCreate((snap, context) => {
+const onSituationCreated = functions.firestore.document('users/{userId}/situations/{situationId}').onCreate((snap, context) => {
     // Get the note document
     const situationData = snap.data()!
 
     // Add an 'objectID' field which Algolia requires
     situationData.objectID = context.params.situationId;
+    situationData.userId = context.params.userId;
 
     // Write to the algolia index
     const index = client.initIndex("situations")
     return index.saveObject(situationData)
 })
 
-const onTaskDataCreated = functions.firestore.document('tasks/{taskId}').onCreate((snap, context) => {
+const onTaskDataCreated = functions.firestore.document('users/{userId}/tasks/{taskId}').onCreate((snap, context) => {
   // Get the note document
   const taskData = snap.data()!
 
   // Add an 'objectID' field which Algolia requires
   taskData.objectID = context.params.taskId;
+  taskData.userId = context.params.userId;
 
   // Write to the algolia index
   const index = client.initIndex("tasks")
