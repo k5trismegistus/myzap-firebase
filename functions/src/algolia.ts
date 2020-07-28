0edee01cf82bacabcbe3ad9eb6f995be5d2a26bf
keigo <k5.trismegistus@gmail.com>
2020-07-29 08:16:08 +0900

    :sparkle: Update completion status of task

--- functions/src/algolia.ts
+++ functions/src/algolia.ts
@@ -6,7 +6,6 @@ const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 
 const onSituationCreated = functions.firestore.document('users/{userId}/situations/{situationId}').onCreate((snap, context) => {
-    // Get the note document
     const situationData = snap.data()!
 
     // Add an 'objectID' field which Algolia requires
@@ -19,19 +18,35 @@ const onSituationCreated = functions.firestore.document('users/{userId}/situatio
 })
 
 const onTaskDataCreated = functions.firestore.document('users/{userId}/tasks/{taskId}').onCreate((snap, context) => {
-  // Get the note document
   const taskData = snap.data()!
 
   // Add an 'objectID' field which Algolia requires
   taskData.objectID = context.params.taskId;
   taskData.userId = context.params.userId;
+  taskData.completed = false;
 
   // Write to the algolia index
   const index = client.initIndex("tasks")
   return index.saveObject(taskData)
 })
 
+const onTaskDataChanged = functions.firestore.document('users/{userId}/tasks/{taskId}').onUpdate((change, context) => {
+  // const before = change.before.data()!
+  const after = change.after.data()!
+  const index = client.initIndex('tasks')
+
+  if (after.completion) {
+    index.partialUpdateObject({
+      objectID: context.params.taskId,
+      completed: true,
+    }).then((task) => { return task }).catch((err) => { console.error(err) })
+  }
+
+  return
+})
+
 export {
   onSituationCreated,
   onTaskDataCreated,
+  onTaskDataChanged,
 }
